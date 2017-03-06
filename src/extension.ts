89924d856b0cfe3fa2fe10515cfb949c031885ff
James Pogran <james.pogran@puppetlabs.com>
2017-03-06 14:33:38 -0500

    wip linter eventing

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import ChildProcess = cp.ChildProcess;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,19 +13,80 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-puppet" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.puppetLint', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
-  });
-
-  context.subscriptions.push(disposable);
+  let lint = new PuppetLintProvider();
+  let pcontroller = new PuppetLintController(lint);
+  context.subscriptions.push(pcontroller);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class PuppetLintController {
+
+  private _lintProvider: PuppetLintProvider;
+  private _disposable: vscode.Disposable;
+
+  private diagnosticCollection: vscode.DiagnosticCollection;
+
+  constructor(lintProvider: PuppetLintProvider) {
+
+    this.diagnosticCollection = vscode.languages.createDiagnosticCollection();
+
+    this._lintProvider = lintProvider;
+
+    let subscriptions: vscode.Disposable[] = [];
+
+    vscode.workspace.onDidOpenTextDocument(this._lintProvider.lint, this, subscriptions);
+    vscode.workspace.onDidSaveTextDocument(this._lintProvider.lint, this);
+    vscode.workspace.onDidCloseTextDocument((textDocument)=> {
+      this.diagnosticCollection.delete(textDocument.uri);
+    }, null, subscriptions);
+
+    vscode.workspace.textDocuments.forEach(this._lintProvider.lint, this);
+
+    this._disposable = vscode.Disposable.from(...subscriptions);
+  }
+
+  dispose() {
+    this._disposable.dispose();
+
+    this.diagnosticCollection.clear();
+    this.diagnosticCollection.dispose();
+  }
+}
+
+class PuppetLintProvider{
+  private diagnosticCollection: vscode.DiagnosticCollection;
+
+  lint(textDocument: any){
+    if(textDocument.languageId !== 'puppet') return;
+
+    let fileName = textDocument.fileName;
+
+    let text = '';
+    let diagnostics: vscode.Diagnostic[] = [];
+    let options = vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
+    let proc = cp.spawn('puppet-lint',
+              ["--log-format", "%{KIND}:%{line}:%{message}", fileName], options);
+    if (proc.pid) {
+      proc.stdout.on('data', (data: Buffer) => {
+        text += data;
+      })
+      proc.stdout.on('end', ()=> {
+        text.split("\n").forEach(line=>{
+          if(line){
+            let fields = line.split(":");
+            let severity = fields[0].toLowerCase() === "warning" ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Error;
+            let message = "puppet-lint: " + fields[0] + ": " + fields[2];
+            let range = new vscode.Range(Number(fields[1]) - 1, 0, Number(fields[1]) -1, 300);
+            let diagnostic = new vscode.Diagnostic(range, message, severity);
+            diagnostics.push(diagnostic);
+          }
+        })
+        this.diagnosticCollection.set(textDocument.uri, diagnostics);
+      });
+    }
+  }
+
 }
