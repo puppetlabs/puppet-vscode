95a0981cc2fa8a23b43121f20cd6afa65cb6d8f5
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-05-12 16:52:20 -0700

    Add Language Server to the Extension
    Previously the extension did not instantiate a language server client.  This
    commit adds a language server client (currently hardcoded to 127.0.0.1). This
    commit also removes the duplicate commands of lint and validate from the
    extension as these are now implemented natively by the language server.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,14 +2,44 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as net from 'net';
 import * as cp from 'child_process';
 import ChildProcess = cp.ChildProcess;
 
-import { puppetLintCommand } from '../src/commands/puppetLintCommand';
 import { puppetResourceCommand } from '../src/commands/puppetResourceCommand';
 import { puppetModuleCommand } from '../src/commands/puppetModuleCommand';
-import { puppetLintProvider } from '../src/providers/puppetLintProvider';
-import { puppetParserValidateProvider } from '../src/providers/puppetParserValidateProvider';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient';
+
+function startLangServerTCP(host: string, addr: number, documentSelector: string | string[]): vscode.Disposable {
+  let serverOptions: ServerOptions = function() {
+    return new Promise((resolve, reject) => {
+      var client = new net.Socket();
+      client.connect(addr, host, function() {
+        resolve({
+          reader: client,
+          writer: client
+        });
+      });
+      client.on('error', function(err) {
+        console.log(`[Puppet Lang Server Client] #{err}`);
+      })
+
+    });
+  }
+
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for puppet manifests
+    documentSelector: ['puppet'],
+    // synchronize: {
+    //   // Synchronize the setting section 'languageServerExample' to the server
+    //   configurationSection: 'languageServerExample',
+    //   // Notify the server about file changes to '.clientrc files contain in the workspace
+    //   fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+    // }
+  }
+
+  return new LanguageClient(`tcp lang server (host ${host} port ${addr})`, serverOptions, clientOptions).start();
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,20 +48,6 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "vscode-puppet" is now active!');
-
-  let linter = new puppetLintProvider();
-  linter.activate(context.subscriptions);
-
-  let validator = new puppetParserValidateProvider();
-  validator.activate(context.subscriptions);
-
-
-  let lintCommand = new puppetLintCommand();
-  var ldisposable = vscode.commands.registerCommand('extension.puppetLint', () => {
-    lintCommand.fixDocument();
-  });
-  context.subscriptions.push(lintCommand);
-  context.subscriptions.push(ldisposable);
 
   let resourceCommand = new puppetResourceCommand();
   var rdisposable = vscode.commands.registerCommand('extension.puppetResource', () => {
@@ -48,6 +64,9 @@ export function activate(context: vscode.ExtensionContext) {
   });
   context.subscriptions.push(moduleCommand);
   context.subscriptions.push(rdisposable);
+
+  // TCP Language Server
+  context.subscriptions.push(startLangServerTCP('127.0.0.1', 8081, ["puppet"]));
 }
 
 // this method is called when your extension is deactivated
