6897b77cd3e2cc8b53f99634bfd589635faa651f
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-05-22 14:41:10 -0700

    Add integration tests for Completion Provider list
    This commit adds integration tests for the Completion Provider when providing
    a completion list.

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -6,3 +6,17 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'..','lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'..','vendor','puppet-lint','lib'))
 
 require 'puppet-languageserver'
+PuppetLanguageServer::InitPuppet(PuppetLanguageServer::CommandLineParser.parse([]))
+
+# Custom RSpec Matchers
+
+RSpec::Matchers.define :be_completion_item_with_type do |value|
+  value = [value] unless value.is_a?(Array)
+
+  match { |actual| value.include?(actual['data']['type']) }
+
+  description do
+    "be a Completion Item with a data type in the list of #{value}"
+  end
+end
+
