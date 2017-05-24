8c9da50174946ed6201ce950cd2acee67994b483
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-05-23 21:48:11 -0700

    Update lib/puppet-languageserver/document_validator for rubocop
    Previously many ruby files contained many rubocop violations.  This commit
    updates the lib/puppet-languageserver/document_validator.rb file and resolves outstanding
    violations

--- server/lib/puppet-languageserver/document_validator.rb
+++ server/lib/puppet-languageserver/document_validator.rb
@@ -1,9 +1,9 @@
 require 'puppet-lint'
 module PuppetLanguageServer
-
   module DocumentValidator
-    def self.validate(content, max_problems = 100)
+    def self.validate(content, _max_problems = 100)
       result = []
+      # TODO: Need to implement max_problems
       problems = 0
 
       begin
@@ -11,38 +11,33 @@ module PuppetLanguageServer
         problems = linter.run(nil, content)
         unless problems.nil?
           problems.each do |problem|
-            if problem[:kind] == :error && problem[:check] == :syntax
-              # Syntax errors are better handled by the puppet parser, not puppet lint
-              use_puppet_parser = true
-              next
-            end
+            # Syntax errors are better handled by the puppet parser, not puppet lint
+            next if problem[:kind] == :error && problem[:check] == :syntax
 
             severity = case problem[:kind]
-              when :error
-                LanguageServer::DIAGNOSTICSEVERITY_ERROR
-              when :warning
-                LanguageServer::DIAGNOSTICSEVERITY_WARNING
-              else
-                LanguageServer::DIAGNOSTICSEVERITY_HINT
-            end
+                       when :error
+                         LanguageServer::DIAGNOSTICSEVERITY_ERROR
+                       when :warning
+                         LanguageServer::DIAGNOSTICSEVERITY_WARNING
+                       else
+                         LanguageServer::DIAGNOSTICSEVERITY_HINT
+                       end
 
             endpos = problem[:column] - 1
             endpos = problem[:column] - 1 + problem[:token].to_manifest.length unless problem[:token].nil? || problem[:token].value.nil?
 
-          result << LanguageServer::Diagnostic.create({
-            'severity' => severity,
-            'code' => problem[:check].to_s,
-            'fromline' => problem[:line] - 1,   # Line numbers from puppet are base 1
-            'toline' => problem[:line] - 1,     # Line numbers from puppet are base 1
-            'fromchar' => problem[:column] - 1, # Pos numbers from puppet are base 1
-            'tochar' => endpos,
-            'source' => 'Puppet',
-            'message' => problem[:message],
-          })
-
+            result << LanguageServer::Diagnostic.create('severity' => severity,
+                                                        'code' => problem[:check].to_s,
+                                                        'fromline' => problem[:line] - 1,   # Line numbers from puppet are base 1
+                                                        'toline' => problem[:line] - 1,     # Line numbers from puppet are base 1
+                                                        'fromchar' => problem[:column] - 1, # Pos numbers from puppet are base 1
+                                                        'tochar' => endpos,
+                                                        'source' => 'Puppet',
+                                                        'message' => problem[:message])
           end
         end
-      rescue => exception
+      # rubocop:disable Lint/HandleExceptions
+      rescue => _exception
         # If anything catastrophic happens we resort to puppet parsing anyway
       end
 
@@ -50,9 +45,9 @@ module PuppetLanguageServer
       Puppet[:code] = content
       env = Puppet.lookup(:current_environment)
       loaders = Puppet::Pops::Loaders.new(env)
-      Puppet.override( {:loaders => loaders } , _('For puppet parser validate')) do
+      Puppet.override({ :loaders => loaders }, _('For puppet parser validate')) do
         begin
-          validation_environment = nil ? env.override_with(:manifest => nil) : env
+          validation_environment = env
           validation_environment.check_for_reparse
           validation_environment.known_resource_types.clear
         rescue => detail
@@ -63,15 +58,13 @@ module PuppetLanguageServer
           message = detail.basic_message if message.nil? && detail.respond_to?(:basic_message)
 
           unless detail.line.nil? || detail.pos.nil? || message.nil?
-            result << LanguageServer::Diagnostic.create({
-              'severity' => LanguageServer::DIAGNOSTICSEVERITY_ERROR,
-              'fromline' => detail.line - 1,  # Line numbers from puppet are base 1
-              'toline' => detail.line - 1,    # Line numbers from puppet are base 1
-              'fromchar' => detail.pos - 1,   # Pos numbers from puppet are base 1
-              'tochar' => detail.pos + 1 - 1, # Pos numbers from puppet are base 1
-              'source' => 'Puppet',
-              'message' => message,
-            })
+            result << LanguageServer::Diagnostic.create('severity' => LanguageServer::DIAGNOSTICSEVERITY_ERROR,
+                                                        'fromline' => detail.line - 1,  # Line numbers from puppet are base 1
+                                                        'toline' => detail.line - 1,    # Line numbers from puppet are base 1
+                                                        'fromchar' => detail.pos - 1,   # Pos numbers from puppet are base 1
+                                                        'tochar' => detail.pos + 1 - 1, # Pos numbers from puppet are base 1
+                                                        'source' => 'Puppet',
+                                                        'message' => message)
           end
         end
       end
