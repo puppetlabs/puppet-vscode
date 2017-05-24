2d8d7c9e69338c618e993c69edffe5b3827ac124
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-05-23 21:53:38 -0700

    Update lib/languageserver/hover for rubocop
    Previously many ruby files contained many rubocop violations.  This commit
    updates the lib/languageserver/hover.rb file and resolves outstanding
    violations

--- server/lib/languageserver/hover.rb
+++ server/lib/languageserver/hover.rb
@@ -7,9 +7,9 @@ module LanguageServer
   #    * The hover's content
   #    */
   #   contents: MarkedString | MarkedString[];
-    
+
   #   /**
-  #    * An optional range is a range inside a text document 
+  #    * An optional range is a range inside a text document
   #    * that is used to visualize a hover, e.g. by changing the background color.
   #    */
   #   range?: Range;
@@ -20,8 +20,8 @@ module LanguageServer
       result = {}
       raise('contents is a required field for Hover') if options['contents'].nil?
 
-      result['contents'] = options['contents'] 
-      result['range']  = options['range'] unless options['range'].nil?
+      result['contents'] = options['contents']
+      result['range'] = options['range'] unless options['range'].nil?
 
       result
     end
