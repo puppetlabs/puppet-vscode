20cbdc46fa664ebee07eca7338fbdc339c9caf2d
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-06-06 17:02:11 -0700

    Add tests for the Hover Provider and Message Router
    This commit adds tests for the Hover Provider and the Message Router.  This
    commit also adds helper Mock objects for the JSON RPC handler and Pupept
    Resource objects which are needed in unit testing.  The `--no-preload` option
    is added in spec setup to speed up tests which do not require the full Puppet
    initialization, typically these are the unit tests.

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -10,7 +10,7 @@ fixtures_dir = File.join(File.dirname(__FILE__),'fixtures')
 
 # Currently there is no way to re-initialize the puppet loader so for the moment
 # all tests must run off the single puppet config settings instead of per example setting
-server_options = PuppetLanguageServer::CommandLineParser.parse([])
+server_options = PuppetLanguageServer::CommandLineParser.parse(['--no-preload'])
 server_options[:puppet_settings] = ['--vardir',File.join(fixtures_dir,'cache'),
                                     '--confdir',File.join(fixtures_dir,'confdir')]
 PuppetLanguageServer::init_puppet(server_options)
@@ -23,5 +23,51 @@ RSpec::Matchers.define :be_completion_item_with_type do |value|
 
   description do
     "be a Completion Item with a data type in the list of #{value}"
+  end
+end
+
+# Mock ojects
+class MockJSONRPCHandler < PuppetLanguageServer::JSONRPCHandler
+  attr_accessor :socket
+  attr_accessor :simple_tcp_server
+
+  def post_init
+  end
+
+  def unbind
+  end
+
+  def receive_data(data)
+  end
+
+  def error?
+    false
+  end
+
+  def send_data(data)
+    true
+  end
+
+  def close_connection_after_writing
+  end
+
+  def close_connection
+  end
+end
+
+class MockResource
+  attr_accessor :title
+
+  def initialize(type_name = 'type' + rand(65536).to_s, title = 'resource' + rand(65536).to_s)
+    @title = title
+    @type = type_name
+  end
+
+  def to_manifest
+    <<-HEREDOC
+#{@type} { '#{@title}':
+  ensure => present
+}
+    HEREDOC
   end
 end
