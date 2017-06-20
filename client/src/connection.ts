b795664469fa280036ee76320f12178a87cfcd4f
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-06-19 21:27:38 -0700

    (GH-65) Add Windows support to local Language Server
    This commit adds Windows support to the local Language Server. It detects when
    a 32bit VS Code is on a 64bit OS and will use the appropriate file system
    redirection to find a Puppet Agent installation.  This commit also improves the
    user experience and will not throw large errors, but instead warns the user that
    the language server could not be started and has reduced functionality.

--- client/src/connection.ts
+++ client/src/connection.ts
@@ -5,6 +5,7 @@ import cp = require('child_process');
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 import { setupPuppetCommands } from '../src/puppetcommands';
 import * as messages from '../src/messages';
+import fs = require('fs');
 
 const langID = 'puppet'; // don't change this
 
@@ -89,7 +90,13 @@ export class ConnectionManager implements IConnectionManager {
 
     if (this.connectionConfiguration.type == ConnectionType.Local) {
       this.languageServerProcess = this.createLanguageServerProcess(contextPath, this.puppetOutputChannel);
-      if (this.languageServerProcess == undefined) { throw new Error('Unable to start the server process'); }
+      if (this.languageServerProcess == undefined) {
+        if (this.connectionStatus == ConnectionStatus.Failed) {
+          // We've already handled this state.  Just return
+          return
+        }
+        throw new Error('Unable to start the Language Server Process');
+      }
 
       this.languageServerProcess.stdout.on('data', (data) => {
         console.log("OUTPUT: " + data.toString());
@@ -162,7 +169,7 @@ export class ConnectionManager implements IConnectionManager {
   private createLanguageServerProcess(serverExe: string, myOutputChannel: vscode.OutputChannel) {
     myOutputChannel.appendLine('Language server found at: ' + serverExe)
 
-    let cmd = '';
+    let cmd: string = undefined;
     let args = [serverExe];
     let options = {};
 
@@ -176,8 +183,29 @@ export class ConnectionManager implements IConnectionManager {
     //               | 'win32';
     switch (process.platform) {
       case 'win32':
-        myOutputChannel.appendLine('Windows spawn process does not work at the moment')
-        vscode.window.showErrorMessage('Windows spawn process does not work at the moment. Functionality will be limited to syntax highlighting');
+        let comspec: string = process.env["COMSPEC"];
+        let programFiles = process.env["ProgramFiles"];
+        if (process.env["PROCESSOR_ARCHITEW6432"] == "AMD64") {
+          // VSCode is running as 32bit process on a 64bit Operating System.  Need to break out
+          // of the 32bit using the sysnative redirection and environment variables
+          comspec = path.join(process.env["WINDIR"],"sysnative","cmd.exe");
+          programFiles = process.env["ProgramW6432"];
+        }
+        let puppetDir : string = path.join(programFiles,"Puppet Labs","Puppet")
+        let environmentBat : string = path.join(puppetDir,"bin","environment.bat")
+
+        if (!fs.existsSync(puppetDir)) {
+          this.setSessionFailure("Could not find Puppet Agent at " + puppetDir);
+          vscode.window.showWarningMessage('Could not find Puppet Agent installed at "' + puppetDir + '". Functionality will be limited to syntax highlighting');
+          return;
+        }
+
+        cmd = comspec;
+        args = ['/K','CALL',environmentBat,'&&','ruby.exe',serverExe]
+        options = {
+          env: process.env,
+          stdio: 'pipe',
+        };
         break;
       default:
         myOutputChannel.appendLine('Starting language server')
@@ -189,7 +217,14 @@ export class ConnectionManager implements IConnectionManager {
         };
     }
 
+    if (cmd == undefined) {
+      this.setSessionFailure("Unable to start the Language Server on this platform");
+      vscode.window.showWarningMessage('The Puppet Language Server is not supported on this platform (' + process.platform + '). Functionality will be limited to syntax highlighting');
+      return;
+    }
+
     console.log("Starting the language server with " + cmd + " " + args.join(" "));
+    myOutputChannel.appendLine("Starting the language server with " + cmd + " " + args.join(" "));
     var proc = cp.spawn(cmd, args, options)
     console.log("ProcID = " + proc.pid);
     myOutputChannel.appendLine('Language server PID:' + proc.pid)
