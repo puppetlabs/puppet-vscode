cbeef3354f2d75d3195eb66ddeba65b5375e01d1
James Pogran <james.pogran@outlook.com>
2017-09-13 09:58:23 -0400

    (GH-122) Show Upgrade Message
    This commit adds logic to show an upgrade message when an upgrade
    occurs, and gives user an option to supress upgrade messages.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -33,6 +33,11 @@ export class ConnectionConfiguration implements IConnectionConfiguration {
 }
 
 export function activate(context: vscode.ExtensionContext) {
+  const puppetExtension = vscode.extensions.getExtension('jpogran.puppet-vscode')!;
+  const puppetExtensionVersion = puppetExtension.packageJSON.version;
+
+  notifyOnNewExtensionVersion(context, puppetExtensionVersion)
+
   context.subscriptions.push(new Reporter(context));
   var logger = new Logger();
   connManager = new ConnectionManager(context, logger);
@@ -46,5 +51,20 @@ export function deactivate() {
   if (connManager != undefined) {
     connManager.stop();
     connManager.dispose();
+  }
+}
+
+async function notifyOnNewExtensionVersion(context: vscode.ExtensionContext, version: string) {
+  const viewReleaseNotes = 'View Release Notes';
+  const suppressUpdateNotice = 'SuppressUpdateNotice';
+  const dontShowAgainNotice = "Don't show again";
+  
+  if (context.globalState.get(suppressUpdateNotice, false)) return;
+  
+  const result = await vscode.window.showInformationMessage(`Puppet VSCode has been updated to v${version}`, dontShowAgainNotice, undefined, viewReleaseNotes);
+  if (result === viewReleaseNotes) {
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://marketplace.visualstudio.com/items/jpogran.puppet-vscode/changelog'));
+  } else {
+    context.globalState.update(suppressUpdateNotice, true);
   }
 }
