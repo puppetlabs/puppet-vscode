f061e2a89d5501fbc09d2a004d7839e22363d1d9
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-10-02 20:26:04 -0700

    (GH-97) Create a crash file when the language server abends
    Previously when the language server crashed, no debug information would be
    created which would make it difficult to diagnose issues in the field.  This
    commit adds the facility to create a crash file in
    %TMP%\puppet_language_server_crash.txt.
    
    This commit also adds tests for this facility.

--- server/lib/puppet-languageserver/message_router.rb
+++ server/lib/puppet-languageserver/message_router.rb
@@ -19,6 +19,57 @@ module PuppetLanguageServer
       return nil if @documents[uri].nil?
       @documents[uri].clone
     end
+
+    def self.document_uris
+      @documents.keys.dup
+    end
+  end
+
+  module CrashDump
+    def self.default_crash_file
+      File.join(Dir.tmpdir(),'puppet_language_server_crash.txt')
+    end
+
+    def self.write_crash_file(err, filename = nil, additional = {})
+      # Create the crash text
+
+      puppet_version         = Puppet.version rescue 'Unknown'
+      facter_version         = Facter.version rescue 'Unknown'
+      languageserver_version = PuppetLanguageServer.version rescue 'Unknown'
+
+      crashtext = <<-TEXT
+Puppet Language Server Crash File
+-=--=--=--=--=--=--=--=--=--=--=-
+#{DateTime.now.strftime("%a %b %e %Y %H:%M:%S %Z")}
+Puppet Version #{puppet_version}
+Facter Version #{facter_version}
+Ruby Version #{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}
+Language Server Version #{languageserver_version}
+
+Error: #{err.to_s}
+
+Backtrace
+---------
+#{err.backtrace.join("\n") }
+
+TEXT
+      # Append the documents in the cache
+      PuppetLanguageServer::DocumentStore.document_uris.each do |uri|
+        crashtext += "Document - #{uri}\n---\n#{PuppetLanguageServer::DocumentStore.document(uri)}\n\n"
+      end
+      # Append additional objects from the crash
+      additional.each do |k,v|
+        crashtext += "#{k}\n---\n#{v.to_s}\n\n"
+      end
+
+      crash_file = filename.nil? ? default_crash_file : filename
+      File.open(crash_file, 'wb') { |file| file.write(crashtext) }
+    rescue
+      # Swallow all errors.  Errors in the error handler should not
+      # terminate the application
+    end
+
+    nil
   end
 
   class MessageRouter < JSONRPCHandler
@@ -134,6 +185,12 @@ module PuppetLanguageServer
       else
         PuppetLanguageServer.log_message(:error, "Unknown RPC method #{request.rpc_method}")
       end
+    rescue => err
+      PuppetLanguageServer::CrashDump.write_crash_file(err, nil, {
+        'request' => request.rpc_method,
+        'params'  => request.params,
+      })
+      raise
     end
 
     def receive_notification(method, params)
@@ -170,6 +227,12 @@ module PuppetLanguageServer
       else
         PuppetLanguageServer.log_message(:error, "Unknown RPC notification #{method}")
       end
+    rescue => err
+      PuppetLanguageServer::CrashDump.write_crash_file(err, nil, {
+        'notification' => method,
+        'params'  => params,
+      })
+      raise
     end
   end
 end
