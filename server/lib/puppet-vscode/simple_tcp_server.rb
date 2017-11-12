8517ba80f8e1964114e2abe0e043fd9f25d6592e
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-11-11 20:49:33 -0800

    (GH-187) Partial Revert 3690ca20dd
    Previously commit 3690ca20dd the Socket connection object was modified for the
    STDIO server.  However this was causing issues and was not required.  This
    commit partially reverts the changes in 3690ca20dd to the TCP Server and
    JSON Handler to the parent commit.

--- server/lib/puppet-vscode/simple_tcp_server.rb
+++ server/lib/puppet-vscode/simple_tcp_server.rb
@@ -4,8 +4,9 @@ require 'socket'
 # http://stackoverflow.com/questions/29858113/unable-to-make-socket-accept-non-blocking-ruby-2-2
 
 module PuppetVSCode
-  class SocketConnection
+  class SimpleTCPServerConnection
     attr_accessor :socket
+    attr_accessor :simple_tcp_server
 
     # Methods to override
     def post_init
@@ -33,21 +34,6 @@ module PuppetVSCode
       socket.write(data)
       true
     end
-
-    # @api public
-    def close_connection_after_writing
-      true
-    end
-
-    # @api public
-    def close_connection
-      true
-    end
-  end
-
-  class SimpleTCPServerConnection < SocketConnection
-    attr_accessor :socket
-    attr_accessor :simple_tcp_server
 
     # @api public
     def close_connection_after_writing
