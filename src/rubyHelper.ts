cefbe3426730dc8fbd2f6543cc594b4e6cf9311c
jpogran <james.pogran@puppet.com>
2018-04-05 11:55:49 -0400

    (maint) Fix rubyhelper.ts

--- src/rubyHelper.ts
+++ src/rubyHelper.ts
@@ -2,34 +2,38 @@ import path = require('path');
 import fs = require('fs');
 import cp = require('child_process');
 import { ILogger } from './logging';
-import { IConnectionConfiguration, ConnectionType } from './interfaces';
+import { IConnectionConfiguration } from './interfaces';
 
 export class RubyHelper {
-  private static getDirectories(parent) {
-    return fs.readdirSync(parent).filter(function (file) {
+  private static getDirectories(parent:string) {
+    return fs.readdirSync(parent).filter(function(file) {
       return fs.statSync(path.join(parent, file)).isDirectory();
     });
   }
 
   private static pathEnvSeparator() {
-    if (process.platform == 'win32') {
-      return ";";
+    if (process.platform === 'win32') {
+      return ';';
     } else {
-      return ":";
+      return ':';
     }
   }
-  
-  public static getRubyEnvFromPuppetAgent(rubyFile, connectionConfiguration: IConnectionConfiguration, logger: ILogger) {
+
+  public static getRubyEnvFromPuppetAgent(
+    rubyFile:string,
+    connectionConfiguration: IConnectionConfiguration,
+    logger: ILogger
+  ) {
     let logPrefix: string = '[getRubyExecFromPuppetAgent] ';
     // setup defaults
-    let spawn_options: cp.SpawnOptions = {}
+    let spawn_options: cp.SpawnOptions = {};
     spawn_options.env = process.env;
     let result = {
       command: 'ruby',
       args: [rubyFile],
-      options: spawn_options,
-    }
-    let puppetAgentDir: string = null;
+      options: spawn_options
+    };
+    let puppetAgentDir: string = '';
 
     // type Platform = 'aix'
     //               | 'android'
@@ -41,17 +45,13 @@ export class RubyHelper {
     //               | 'win32';
     switch (process.platform) {
       case 'win32':
-        let comspec: string = process.env["COMSPEC"];
-        let programFiles = process.env["ProgramFiles"];
-        if (process.env["PROCESSOR_ARCHITEW6432"] == "AMD64") {
-          // VSCode is running as 32bit process on a 64bit Operating System.  Need to break out
-          // of the 32bit using the sysnative redirection and environment variables
-          comspec = path.join(process.env["WINDIR"],"sysnative","cmd.exe");
-          programFiles = process.env["ProgramW6432"];
+        let programFiles = process.env['ProgramFiles'];
+        if (process.env['PROCESSOR_ARCHITEW6432'] === 'AMD64') {
+          programFiles = process.env['ProgramW6432'];
         }
 
-        if (connectionConfiguration.puppetAgentDir == undefined) {
-          puppetAgentDir = path.join(programFiles, "Puppet Labs", "Puppet");
+        if (connectionConfiguration.puppetAgentDir === undefined) {
+          puppetAgentDir = path.join(programFiles, 'Puppet Labs', 'Puppet'); // tslint:disable-line
         } else {
           puppetAgentDir = connectionConfiguration.puppetAgentDir;
         }
@@ -59,7 +59,7 @@ export class RubyHelper {
         result.options.stdio = 'pipe';
         break;
       default:
-        if (connectionConfiguration.puppetAgentDir == undefined) {
+        if (connectionConfiguration.puppetAgentDir === undefined) {
           puppetAgentDir = '/opt/puppetlabs/puppet';
         } else {
           puppetAgentDir = connectionConfiguration.puppetAgentDir;
@@ -70,51 +70,75 @@ export class RubyHelper {
         break;
     }
 
-    let puppetDir = path.join(puppetAgentDir,"puppet");
-    let facterDir = path.join(puppetAgentDir,"facter");
-    let hieraDir = path.join(puppetAgentDir,"hiera");
-    let mcoDir = path.join(puppetAgentDir,"mcollective");
-    let rubydir = path.join(puppetAgentDir,"sys","ruby");
-    let rubylib = path.join(puppetDir,"lib") + this.pathEnvSeparator() + path.join(facterDir,"lib") + this.pathEnvSeparator() + path.join(hieraDir,"lib") + this.pathEnvSeparator() + path.join(mcoDir,"lib")
+    let puppetDir = path.join(puppetAgentDir, 'puppet');
+    let facterDir = path.join(puppetAgentDir, 'facter');
+    let hieraDir = path.join(puppetAgentDir, 'hiera');
+    let mcoDir = path.join(puppetAgentDir, 'mcollective');
+    let rubydir = path.join(puppetAgentDir, 'sys', 'ruby');
+    let rubylib =
+      path.join(puppetDir, 'lib') +
+      this.pathEnvSeparator() +
+      path.join(facterDir, 'lib') +
+      this.pathEnvSeparator() +
+      path.join(hieraDir, 'lib') +
+      this.pathEnvSeparator() +
+      path.join(mcoDir, 'lib');
 
-    if (process.platform == 'win32') {
+    if (process.platform === 'win32') {
       // Translate all slashes to / style to avoid puppet/ruby issue #11930
-      rubylib = rubylib.replace(/\\/g,"/");
+      rubylib = rubylib.replace(/\\/g, '/');
     }
 
     // Setup the process environment variables
-    if (result.options.env.PATH == undefined) { result.options.env.PATH = ''; }
-    if (result.options.env.RUBYLIB == undefined) { result.options.env.RUBYLIB = ''; }
+    if (result.options.env.PATH === undefined) {
+      result.options.env.PATH = '';
+    }
+    if (result.options.env.RUBYLIB === undefined) {
+      result.options.env.RUBYLIB = '';
+    }
     result.options.env.RUBY_DIR = rubydir;
-    result.options.env.PATH = path.join(puppetDir,"bin") + this.pathEnvSeparator() + path.join(facterDir,"bin") + this.pathEnvSeparator() + path.join(hieraDir,"bin") + this.pathEnvSeparator() + path.join(mcoDir,"bin") +
-                              this.pathEnvSeparator() + path.join(puppetAgentDir,"bin") + this.pathEnvSeparator() + path.join(rubydir,"bin") + this.pathEnvSeparator() + path.join(puppetAgentDir,"sys","tools","bin") +
-                              this.pathEnvSeparator() + result.options.env.PATH;
+    result.options.env.PATH =
+      path.join(puppetDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(facterDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(hieraDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(mcoDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(puppetAgentDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(rubydir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(puppetAgentDir, 'sys', 'tools', 'bin') +
+      this.pathEnvSeparator() +
+      result.options.env.PATH;
     result.options.env.RUBYLIB = rubylib + this.pathEnvSeparator() + result.options.env.RUBYLIB;
     result.options.env.RUBYOPT = 'rubygems';
-    result.options.env.SSL_CERT_FILE = path.join(puppetDir,"ssl","cert.pem");
-    result.options.env.SSL_CERT_DIR = path.join(puppetDir,"ssl","certs");
+    result.options.env.SSL_CERT_FILE = path.join(puppetDir, 'ssl', 'cert.pem');
+    result.options.env.SSL_CERT_DIR = path.join(puppetDir, 'ssl', 'certs');
 
-    logger.debug(logPrefix + "Using environment variable RUBY_DIR=" + result.options.env.RUBY_DIR);
-    logger.debug(logPrefix + "Using environment variable PATH=" + result.options.env.PATH);
-    logger.debug(logPrefix + "Using environment variable RUBYLIB=" + result.options.env.RUBYLIB);
-    logger.debug(logPrefix + "Using environment variable RUBYOPT=" + result.options.env.RUBYOPT);
-    logger.debug(logPrefix + "Using environment variable SSL_CERT_FILE=" + result.options.env.SSL_CERT_FILE);
-    logger.debug(logPrefix + "Using environment variable SSL_CERT_DIR=" + result.options.env.SSL_CERT_DIR);
+    logger.debug(logPrefix + 'Using environment variable RUBY_DIR=' + result.options.env.RUBY_DIR);
+    logger.debug(logPrefix + 'Using environment variable PATH=' + result.options.env.PATH);
+    logger.debug(logPrefix + 'Using environment variable RUBYLIB=' + result.options.env.RUBYLIB);
+    logger.debug(logPrefix + 'Using environment variable RUBYOPT=' + result.options.env.RUBYOPT);
+    logger.debug(logPrefix + 'Using environment variable SSL_CERT_FILE=' + result.options.env.SSL_CERT_FILE);
+    logger.debug(logPrefix + 'Using environment variable SSL_CERT_DIR=' + result.options.env.SSL_CERT_DIR);
 
     return result;
   }
 
-  public static getRubyEnvFromPDK(rubyFile, connectionConfiguration: IConnectionConfiguration, logger: ILogger) {
+  public static getRubyEnvFromPDK(rubyFile:string, connectionConfiguration: IConnectionConfiguration, logger: ILogger) {
     let logPrefix: string = '[getRubyEnvFromPDK] ';
     // setup defaults
-    let spawn_options: cp.SpawnOptions = {}
+    let spawn_options: cp.SpawnOptions = {};
     spawn_options.env = process.env;
     let result = {
       command: 'ruby',
       args: [rubyFile],
-      options: spawn_options,
-    }
-    let pdkDir: string = null;
+      options: spawn_options
+    };
+    let pdkDir: string = '';
 
     // type Platform = 'aix'
     //               | 'android'
@@ -126,16 +150,12 @@ export class RubyHelper {
     //               | 'win32';
     switch (process.platform) {
       case 'win32':
-        let comspec: string = process.env["COMSPEC"];
-        let programFiles = process.env["ProgramFiles"];
-        if (process.env["PROCESSOR_ARCHITEW6432"] == "AMD64") {
-          // VSCode is running as 32bit process on a 64bit Operating System.  Need to break out
-          // of the 32bit using the sysnative redirection and environment variables
-          comspec = path.join(process.env["WINDIR"],"sysnative","cmd.exe");
-          programFiles = process.env["ProgramW6432"];
+        let programFiles = process.env['ProgramFiles'];
+        if (process.env['PROCESSOR_ARCHITEW6432'] === 'AMD64') {
+          programFiles = process.env['ProgramW6432'];
         }
 
-        pdkDir = path.join(programFiles, "Puppet Labs", "DevelopmentKit");
+        pdkDir = path.join(programFiles, 'Puppet Labs', 'DevelopmentKit'); // tslint:disable-line
 
         result.options.stdio = 'pipe';
         break;
@@ -147,47 +167,60 @@ export class RubyHelper {
         break;
     }
     // Check if this really is a PDK installation
-    if (!fs.existsSync(path.join(pdkDir, "PDK_VERSION"))) {
-      logger.debug(logPrefix + "Could not find a valid PDK installation at " + pdkDir);
+    if (!fs.existsSync(path.join(pdkDir, 'PDK_VERSION'))) {
+      logger.debug(logPrefix + 'Could not find a valid PDK installation at ' + pdkDir);
       return null;
     } else {
-      logger.debug(logPrefix + "Found a valid PDK installation at " + pdkDir);
+      logger.debug(logPrefix + 'Found a valid PDK installation at ' + pdkDir);
     }
-      
+
     // Now to detect ruby versions
-    let subdirs = this.getDirectories(path.join(pdkDir,"private", "ruby"));
-    if (subdirs.length == 0) { return null; }
-    let rubyDir = path.join(pdkDir,"private", "ruby",subdirs[0]);
+    let subdirs = this.getDirectories(path.join(pdkDir, 'private', 'ruby'));
+    if (subdirs.length === 0) {
+      return null;
+    }
+    let rubyDir = path.join(pdkDir, 'private', 'ruby', subdirs[0]);
 
-    subdirs = this.getDirectories(path.join(pdkDir,"share","cache","ruby"));
-    if (subdirs.length == 0) { return null; }
-    let gemDir = path.join(pdkDir,"share","cache","ruby",subdirs[0]);
+    subdirs = this.getDirectories(path.join(pdkDir, 'share', 'cache', 'ruby'));
+    if (subdirs.length === 0) {
+      return null;
+    }
+    let gemDir = path.join(pdkDir, 'share', 'cache', 'ruby', subdirs[0]);
 
-    let rubylib = path.join(pdkDir,'lib')
-    if (process.platform == 'win32') {
+    let rubylib = path.join(pdkDir, 'lib');
+    if (process.platform === 'win32') {
       // Translate all slashes to / style to avoid puppet/ruby issue #11930
-      rubylib = rubylib.replace(/\\/g,"/");
-      gemDir = gemDir.replace(/\\/g,"/");
+      rubylib = rubylib.replace(/\\/g, '/');
+      gemDir = gemDir.replace(/\\/g, '/');
     }
 
     // Setup the process environment variables
-    if (result.options.env.PATH == undefined) { result.options.env.PATH = '' }
-    if (result.options.env.RUBYLIB == undefined) { result.options.env.RUBYLIB = '' }
-    
+    if (result.options.env.PATH === undefined) {
+      result.options.env.PATH = '';
+    }
+    if (result.options.env.RUBYLIB === undefined) {
+      result.options.env.RUBYLIB = '';
+    }
+
     result.options.env.RUBY_DIR = rubyDir;
-    result.options.env.PATH = path.join(pdkDir,'bin') + this.pathEnvSeparator() + path.join(rubyDir,'bin') + this.pathEnvSeparator() + result.options.env.PATH;
-    result.options.env.RUBYLIB = path.join(pdkDir,'lib') + this.pathEnvSeparator() + result.options.env.RUBYLIB;
+    result.options.env.PATH =
+      path.join(pdkDir, 'bin') +
+      this.pathEnvSeparator() +
+      path.join(rubyDir, 'bin') +
+      this.pathEnvSeparator() +
+      result.options.env.PATH;
+    result.options.env.RUBYLIB = path.join(pdkDir, 'lib') + this.pathEnvSeparator() + result.options.env.RUBYLIB;
     result.options.env.GEM_PATH = gemDir;
     result.options.env.GEM_HOME = gemDir;
     result.options.env.RUBYOPT = 'rubygems';
 
-    logger.debug(logPrefix + "Using environment variable RUBY_DIR=" + result.options.env.RUBY_DIR);
-    logger.debug(logPrefix + "Using environment variable PATH=" + result.options.env.PATH);
-    logger.debug(logPrefix + "Using environment variable RUBYLIB=" + result.options.env.RUBYLIB);
-    logger.debug(logPrefix + "Using environment variable GEM_PATH=" + result.options.env.GEM_PATH);
-    logger.debug(logPrefix + "Using environment variable GEM_HOME=" + result.options.env.GEM_HOME);
-    logger.debug(logPrefix + "Using environment variable RUBYOPT=" + result.options.env.RUBYOPT);
-    
+    logger.debug(logPrefix + 'Using environment variable RUBY_DIR=' + result.options.env.RUBY_DIR);
+    logger.debug(logPrefix + 'Using environment variable PATH=' + result.options.env.PATH);
+    logger.debug(logPrefix + 'Using environment variable RUBYLIB=' + result.options.env.RUBYLIB);
+    logger.debug(logPrefix + 'Using environment variable GEM_PATH=' + result.options.env.GEM_PATH);
+    logger.debug(logPrefix + 'Using environment variable GEM_HOME=' + result.options.env.GEM_HOME);
+    logger.debug(logPrefix + 'Using environment variable RUBYOPT=' + result.options.env.RUBYOPT);
+
     return result;
   }
 }
