3ca64faeebc5a2d9e67651075a77b83c159ba344
jpogran <james.pogran@puppet.com>
2018-04-05 10:35:17 -0400

    (maint) Fix extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,31 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as path from 'path';
 
-import { IConnectionConfiguration, ConnectionType } from './interfaces';
 import { ConnectionManager } from './connection';
 import { ConnectionConfiguration } from './configuration';
-import { ILogger } from './logging';
 import { OutputChannelLogger } from './logging/outputchannel';
 import { Reporter } from './telemetry/telemetry';
 
-const langID = 'puppet'; // don't change this
-var statusBarItem;
-var serverProc;
-
-var connManager: ConnectionManager = undefined;
+var connManager: ConnectionManager;
 
 export function activate(context: vscode.ExtensionContext) {
   const puppetExtension = vscode.extensions.getExtension('jpogran.puppet-vscode')!;
   const puppetExtensionVersion = puppetExtension.packageJSON.version;
 
-  notifyOnNewExtensionVersion(context, puppetExtensionVersion)
+  notifyOnNewExtensionVersion(context, puppetExtensionVersion);
 
   context.subscriptions.push(new Reporter(context));
+
   var logger = new OutputChannelLogger();
   connManager = new ConnectionManager(context, logger);
-
   var configSettings = new ConnectionConfiguration(context);
+
   connManager.start(configSettings);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
-  if (connManager != undefined) {
+  if (connManager !== undefined) {
     connManager.stop();
     connManager.dispose();
   }
@@ -43,11 +36,26 @@ async function notifyOnNewExtensionVersion(context: vscode.ExtensionContext, ver
   const suppressUpdateNotice = 'SuppressUpdateNotice';
   const dontShowAgainNotice = "Don't show again";
 
-  if (context.globalState.get(suppressUpdateNotice, false)) return;
+  if (context.globalState.get(suppressUpdateNotice, false)) {
+    return;
+  }
 
-  const result = await vscode.window.showInformationMessage(`Puppet VSCode has been updated to v${version}`, dontShowAgainNotice, undefined, viewReleaseNotes);
-  if (result === viewReleaseNotes) {
-    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://marketplace.visualstudio.com/items/jpogran.puppet-vscode/changelog'));
+  const result = await vscode.window.showInformationMessage(
+    `Puppet VSCode has been updated to v${version}`,
+    { modal: false },
+    { title: dontShowAgainNotice },
+    { title: viewReleaseNotes }
+  );
+
+  if (result === undefined) {
+    return;
+  }
+
+  if (result.title === viewReleaseNotes) {
+    vscode.commands.executeCommand(
+      'vscode.open',
+      vscode.Uri.parse('https://marketplace.visualstudio.com/items/jpogran.puppet-vscode/changelog')
+    );
   } else {
     context.globalState.update(suppressUpdateNotice, true);
   }
