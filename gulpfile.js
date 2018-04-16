65ec07d13c62fb544723cc79199990c4f73c8b20
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-04-16 20:38:09 +0800

    (GH-258) Add vendoring process for Editor Services
    Now that the server and client have been split into separate git repos the
    vendoring process will no longer work.  This commit updates the gulp tasks to
    take in a configuartion file (editor-services.json) to pull in the editor
    services in preparation for being used by the Puppet extension.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,35 @@ var runSequence = require('run-sequence');
 var exec = require('child_process').exec;
 var bump = require('gulp-bump');
 var args = require('yargs').argv;
+var download = require('gulp-downloader');
+var decompress = require('gulp-decompress');
+var path = require('path');
+
+var editorServicesConfig = undefined
+function getEditorServicesConfig() {
+  if (editorServicesConfig != undefined) { return editorServicesConfig }
+  // Read the file and parse its contents.
+  var fs = require('fs');
+
+  filename = path.join(__dirname, 'editor-services.json');
+  content = fs.readFileSync(filename, 'utf8');
+  editorServicesConfig = JSON.parse(content);
+
+  return editorServicesConfig;
+}
+
+function getEditorServicesReleaseURL(config) {
+  var githubuser = (config.githubuser == undefined) ? 'lingua-pupuli' : config.githubuser
+  var githubrepo = (config.githubrepo == undefined) ? 'puppet-editor-services' : config.githubrepo
+  var releasenumber = config.release;
+
+  if (releasenumber == undefined) {
+    throw "The name/number of the release must be set"
+  };
+
+  // Example URL https://github.com/lingua-pupuli/puppet-editor-services/releases/download/0.10.0/puppet_editor_services_0.10.0.zip
+  return `https://github.com/${githubuser}/${githubrepo}/releases/download/${releasenumber}/puppet_editor_services_${releasenumber}.zip`;
+};
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['build']);
@@ -13,14 +42,33 @@ gulp.task('clean', function () {
   return del(['vendor'])
 })
 
-gulp.task('copy_language_server', function () {
-  return gulp.src(['../server/lib/**/*',
-                   '../server/vendor/**/*',
-                   '../server/puppet-debugserver',
-                   '../server/puppet-languageserver'
-                  ], { base: '../server'})
-             .pipe(gulp.dest('./vendor/languageserver'));
-})
+gulp.task('vendor_editor_services', function (callback) {
+  var config = getEditorServicesConfig();
+
+  // Use the github releases url if 'release' is defined
+  if (config.release != undefined) {
+    return download({
+      fileName: 'editor_services.zip',
+      request: {
+        url: getEditorServicesReleaseURL(config)
+      }
+    })
+    .pipe(decompress())
+    .pipe(gulp.dest('./vendor/languageserver'));
+  }
+
+  // Use a simple filecopy if 'directory' is defined
+  if (config.directory != undefined) {
+    return gulp.src([path.join(config.directory,'lib/**/*'),
+                     path.join(config.directory,'vendor/**/*'),
+                     path.join(config.directory,'puppet-debugserver'),
+                     path.join(config.directory,'puppet-languageserver')
+                    ], { base: path.join(config.directory) })
+              .pipe(gulp.dest('./vendor/languageserver'));
+  }
+
+  throw "Unable to vendor Editor Serices.  Missing a release or directory configuration item"
+});
 
 gulp.task('compile_typescript', function (callback) {
   exec('tsc -p ./',
@@ -31,9 +79,8 @@ gulp.task('compile_typescript', function (callback) {
     });
 })
 
-
 gulp.task('build', function (callback) {
-  runSequence('clean','copy_language_server','compile_typescript',callback);
+  runSequence('clean','vendor_editor_services','compile_typescript',callback);
 })
 
 gulp.task('bump', function () {
