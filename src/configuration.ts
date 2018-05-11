2f4103b76a66206f6262bfbb600365d15eedcd2c
jpogran <james.pogran@puppet.com>
2018-05-11 13:55:44 -0400

    (maint) Fix tslint for configuration.ts
    This commit fixes tslint errors

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,26 +3,25 @@
 import * as vscode from 'vscode';
 
 import { IConnectionConfiguration, ConnectionType } from './interfaces';
-import { ConnectionManager } from './connection';
 
 export class ConnectionConfiguration implements IConnectionConfiguration {
   public type: ConnectionType = ConnectionType.Unknown;
-  public host: string = undefined;
-  public port: number = undefined;
-  public timeout: number = undefined;
-  public enableFileCache: boolean = undefined;
-  public debugFilePath: string = undefined;
-  public puppetAgentDir: string = undefined;
-  public langID = 'puppet'; // don't change this
+  public host: string;
+  public port: number;
+  public timeout: number;
+  public enableFileCache: boolean;
+  public debugFilePath: string;
+  public puppetAgentDir: string;
+  public langID: string = 'puppet'; // don't change this
 
   constructor(context: vscode.ExtensionContext) {
     let config = vscode.workspace.getConfiguration('puppet');
 
-    this.host            = config['languageserver']['address'];
-    this.port            = config['languageserver']['port'];
-    this.timeout         = config['languageserver']['timeout'];
+    this.host = config['languageserver']['address'];
+    this.port = config['languageserver']['port'];
+    this.timeout = config['languageserver']['timeout'];
     this.enableFileCache = config['languageserver']['filecache']['enable'];
-    this.debugFilePath   = config['languageserver']['debugFilePath'];
+    this.debugFilePath = config['languageserver']['debugFilePath'];
 
     this.puppetAgentDir = config['puppetAgentDir'];
   }
