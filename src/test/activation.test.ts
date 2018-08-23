f6a1b9e8396b2267be97243cbb49ca38cf57dc37
jpogran <james.pogran@puppet.com>
2018-08-23 15:06:14 -0400

    working tests, still not waiting on language server

--- src/test/activation.test.ts
+++ src/test/activation.test.ts
@@ -4,55 +4,56 @@ import * as vscode from 'vscode';
 import * as assert from 'assert';
 import { getDocUri, activate } from './helper';
 
-describe('Should do completion', () => {
-  
+describe('Should do completion', function() {
   const docUri = getDocUri('empty.pp');
-  before(()=>{
-    activate(docUri);
+  var doc: vscode.TextDocument;
+  var editor: vscode.TextEditor;
+  var documentEol: string;
+  var platformEol: string;
+  function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  before(async () => {
+    // The extensionId is `publisher.name` from package.json
+    const ext = vscode.extensions.getExtension('jpogran.puppet-vscode');
+    await ext.activate();
+    try {
+      doc = await vscode.workspace.openTextDocument(docUri);
+      editor = await vscode.window.showTextDocument(doc);
+      
+      await sleep(2000); // Wait for server activation
+    } catch (e) {
+      console.error(e);
+    }
   });
 
   it('Completes simple snippets Puppet in txt file', async () => {
-    // await activate(docUri);
-    await testCompletion(docUri, new vscode.Position(0, 0), {
+    var position = new vscode.Position(0, 0);
+
+    var expectedCompletionList = {
       items: [
+        // base snippets completion
         { label: 'case', kind: vscode.CompletionItemKind.Snippet },
         { label: 'if', kind: vscode.CompletionItemKind.Snippet },
-        { label: 'unless', kind: vscode.CompletionItemKind.Snippet }
+        { label: 'unless', kind: vscode.CompletionItemKind.Snippet },
+        // puppet language server completion
+        // { label: 'user', kind: vscode.CompletionItemKind.Snippet }
       ]
+    };
+
+    // Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
+    const actualCompletionList = (await vscode.commands.executeCommand(
+      'vscode.executeCompletionItemProvider',
+      docUri,
+      position
+    )) as vscode.CompletionList;
+
+    assert.equal(actualCompletionList.items.length, expectedCompletionList.items.length);
+    expectedCompletionList.items.forEach((expectedItem, i) => {
+      const actualItem = actualCompletionList.items[i];
+      assert.equal(actualItem.label, expectedItem.label);
+      assert.equal(actualItem.kind, expectedItem.kind);
     });
   });
-
-  // it('Completes advanced Puppet in txt file', async () => {
-  //   // await activate(docUri);
-  //   await testCompletion(docUri, new vscode.Position(0, 0), {
-  //     items: [
-  //       { label: 'case', kind: vscode.CompletionItemKind.Snippet },
-  //       { label: 'if', kind: vscode.CompletionItemKind.Snippet },
-  //       { label: 'unless', kind: vscode.CompletionItemKind.Snippet },
-  //       { label: 'user', kind: vscode.CompletionItemKind.Snippet },
-  //     ]
-  //   });
-  // });
 });
-
-async function testCompletion(
-  docUri: vscode.Uri,
-  position: vscode.Position,
-  expectedCompletionList: vscode.CompletionList
-) {
-  await activate(docUri);
-
-  // Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
-  const actualCompletionList = (await vscode.commands.executeCommand(
-    'vscode.executeCompletionItemProvider',
-    docUri,
-    position
-  )) as vscode.CompletionList;
-
-  // assert.equal(actualCompletionList.items.length, expectedCompletionList.items.length);
-  expectedCompletionList.items.forEach((expectedItem, i) => {
-    const actualItem = actualCompletionList.items[i];
-    assert.equal(actualItem.label, expectedItem.label);
-    assert.equal(actualItem.kind, expectedItem.kind);
-  });
-}
