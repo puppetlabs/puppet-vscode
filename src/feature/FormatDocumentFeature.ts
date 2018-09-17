ae219925b8c9d5b6407f95799b39ac6559e625aa
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-09-17 16:09:59 +0800

    (GH-373) Feature-ify Format Document provider
    Previously the format document feature and provider was strewn across multple
    files. This commit instead uses a Feature -> Provider pattern to supply the text
    edits.  Functionally there is no difference for the user.

--- src/feature/FormatDocumentFeature.ts
+++ src/feature/FormatDocumentFeature.ts
@@ -1,8 +1,11 @@
 'use strict';
 
-import * as vscode from 'vscode';
-import { ConnectionStatus } from '../interfaces';
+import * as vscode from "vscode";
+import { IFeature } from "../feature";
+import { ILogger } from "../logging";
 import { IConnectionManager } from '../connection';
+import { ConnectionStatus } from '../interfaces';
+import { ISettings } from '../settings';
 import * as messages from '../messages';
 
 class RequestParams implements messages.PuppetFixDiagnosticErrorsRequestParams {
@@ -10,26 +13,59 @@ class RequestParams implements messages.PuppetFixDiagnosticErrorsRequestParams {
   alwaysReturnContent: boolean;
 }
 
-export function PuppetFormatDocumentProvider(document: vscode.TextDocument, options:vscode.FormattingOptions, connMgr: IConnectionManager) : Thenable<vscode.TextEdit[]> {
-  if (connMgr.status != ConnectionStatus.Running ) {
-    vscode.window.showInformationMessage("Formatting Puppet files is not available as the Language Server is not ready");
-    return new Promise((resolve) => { resolve([]); });
+class FormatDocumentProvider {
+  private connectionManager: IConnectionManager = undefined;
+
+  constructor(connectionManager:IConnectionManager) {
+    this.connectionManager = connectionManager;
   }
 
-  let requestParams = new RequestParams;
-  requestParams.documentUri = document.uri.toString(false);
-  requestParams.alwaysReturnContent = false;
-
-  return connMgr.languageClient
-    .sendRequest(messages.PuppetFixDiagnosticErrorsRequest.type, requestParams)
-    .then(
-      (result) => {
-        result = result as messages.PuppetFixDiagnosticErrorsResponse
-        if (result.fixesApplied > 0 && result.newContent != null) {
-          return [vscode.TextEdit.replace(new vscode.Range(0,0, document.lineCount,0), result.newContent)]
-        } else {
-          return []
+  public formatTextEdits(document: vscode.TextDocument, options:vscode.FormattingOptions) : Thenable<vscode.TextEdit[]> {
+    if (this.connectionManager.status !== ConnectionStatus.Running ) {
+      vscode.window.showInformationMessage("Please wait and try again. The Puppet extension is still loading...");
+      return new Promise((resolve) => { resolve([]); });
+    }
+  
+    let requestParams = new RequestParams;
+    requestParams.documentUri = document.uri.toString(false);
+    requestParams.alwaysReturnContent = false;
+  
+    return this.connectionManager.languageClient
+      .sendRequest(messages.PuppetFixDiagnosticErrorsRequest.type, requestParams)
+      .then(
+        (result) => {
+          result = result as messages.PuppetFixDiagnosticErrorsResponse;
+          if (result.fixesApplied > 0 && result.newContent != null) {
+            return [vscode.TextEdit.replace(new vscode.Range(0,0, document.lineCount,0), result.newContent)]
+          } else {
+            return []
+          }
         }
-      }
-    )
+      );
+  }
+}
+
+export class FormatDocumentFeature implements IFeature {
+  private provider: FormatDocumentProvider;
+
+  constructor(
+    langID: string,
+    connectionManager: IConnectionManager,
+    settings: ISettings,
+    logger: ILogger,
+    context: vscode.ExtensionContext
+  ) {
+    this.provider = new FormatDocumentProvider(connectionManager);
+
+    if (settings.format.enable === true) {
+      logger.debug("Registered Format Document provider");
+      context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(langID, {
+        provideDocumentFormattingEdits: (document, options, token) => { return this.provider.formatTextEdits(document, options); }}
+      ));
+    } else {
+      logger.debug("Format Document provider has not been registered");
+    }
+  }
+
+  public dispose(): any { return undefined; }
 }
