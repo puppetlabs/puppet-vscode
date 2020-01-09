fa2ef7c3637cab7fe15f2068fd0c2c21650171f8
James Pogran <james.pogran@outlook.com>
2020-01-09 11:02:30 -0500

    (GH-289) Programmatically Set Language configuration
    This commit programmatically sets language configuration for Puppet
    manifests and Puppetfiles instead of using json files.
    
    This commit also enables the editor to indent properly after a type
    declaration with a title by adding the `onEnterRules` section.

--- src/extension.ts
+++ src/extension.ts
@@ -36,6 +36,8 @@ let extensionFeatures: IFeature[] = [];
 
 export function activate(context: vscode.ExtensionContext) {
   extContext = context;
+  
+  setLanguageConfiguration();
 
   notifyOnNewExtensionVersion(extContext);
 
@@ -290,4 +292,40 @@ async function notifyIfNewPDKVersion(context: vscode.ExtensionContext, settings:
     .catch(error => {
       logger.error(error);
     });
+}
+
+function setLanguageConfiguration() {
+  vscode.languages.setLanguageConfiguration(puppetLangID, {
+    onEnterRules: [
+      {
+        // foo{'bar':}
+        beforeText: /^.*{\s{0,}'.*':/,
+        afterText: /\s{0,}}$/,
+        action: {
+          indentAction: vscode.IndentAction.IndentOutdent
+        }
+      }
+    ],
+    brackets: [
+      ["{", "}"],
+      ["[", "]"],
+      ["(", ")"]
+    ],
+    comments: {
+      lineComment: "#",
+      blockComment: ["/*", "*/"]
+    }
+  });
+  vscode.languages.setLanguageConfiguration(puppetFileLangID, {
+    onEnterRules: [],
+    brackets: [
+      ["{", "}"],
+      ["[", "]"],
+      ["(", ")"]
+    ],
+    comments: {
+      lineComment: "#",
+      blockComment: ["/*", "*/"]
+    }
+  });
 }
