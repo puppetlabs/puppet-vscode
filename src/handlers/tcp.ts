c0c8b87f5a5fe86b1f9e17aaa7af37a180426c9a
James Pogran <james.pogran@outlook.com>
2020-04-28 21:58:54 -0400

    (maint) Fix incorrect tcp protocol detection

--- src/handlers/tcp.ts
+++ src/handlers/tcp.ts
@@ -76,7 +76,7 @@ export class TcpConnectionHandler extends ConnectionHandler {
           this.config.workspace.editorService.tcp.address === 'localhost' ||
           this.config.workspace.editorService.tcp.address === ''
         ) {
-          return ConnectionType.Remote;
+          return ConnectionType.Local;
         } else {
           return ConnectionType.Remote;
         }
