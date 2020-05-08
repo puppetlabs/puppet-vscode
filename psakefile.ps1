3b3451d79124f53244379df6ebd16b94f2d1870b
James Pogran <james.pogran@outlook.com>
2020-05-08 14:12:51 -0400

    (maint) Optimize build script network calls
    This commit optimizes the build script to only pull dependencies like
    npm packages or puppet-editor-services if their files are not present.
    It also changes the vs code launch.json to use the optimized build path,
    which reduces build time when running under the Extension Host.

--- psakefile.ps1
+++ psakefile.ps1
@@ -24,20 +24,21 @@ task Clean {
 #   "githubrepo": "puppet-editor-services",
 #   "githubuser": "glennsarti"
 # }
-  # "editorServices": {
+# "editorServices": {
 #   "githubrepo": "puppet-editor-services",
 #   "githubref": "glennsarti:spike-rearch-langserver"
 # },
-task VendorEditorServices {
+task VendorEditorServices -precondition { !(Test-Path (Join-Path $PSScriptRoot 'vendor/languageserver')) } {
   $githubrepo = $config.editorComponents.editorServices.githubrepo ?? 'puppet-editor-services'
   $githubuser = $config.editorComponents.editorServices.githubuser ?? 'puppetlabs'
 
-  if($config.editorComponents.editorServices.release){
+  if ($config.editorComponents.editorServices.release) {
     $releasenumber = $config.editorComponents.editorServices.release
     $uri = "https://github.com/${githubuser}/${githubrepo}/releases/download/${releasenumber}/puppet_editor_services_${releasenumber}.zip";
-  }else{
+  }
+  else {
     $githubref = $config.editorComponents.editorServices.githubref;
-    if($githubref -notcontains ':'){
+    if ($githubref -notcontains ':') {
       throw "Invalid githubref. Must be in user:branch format like glennsarti:spike-rearch-langserver"
     }
     $githubuser = $githubref.split(":")[0]
@@ -47,7 +48,8 @@ task VendorEditorServices {
 
   if ($config.editorComponents.editorServices.directory) {
     Copy-Item -Path $config.editorComponents.editorServices.directory -Destination $languageServerPath -Recurse -Force
-  }elseif ($config.editorComponents.editorServices.release) {
+  }
+  elseif ($config.editorComponents.editorServices.release) {
     Invoke-RestMethod -Uri $uri -OutFile $languageServerZip -ErrorAction Stop
     Expand-Archive -Path $languageServerZip -DestinationPath $languageServerPath -ErrorAction Stop
     Remove-Item -Path $languageServerZip -Force
@@ -64,19 +66,9 @@ task VendorEditorServices {
   }
 }
 
-task VendorEditorSyntax {
-  if ($config.editorComponents.editorSyntax.githubuser) {
-    $githubuser = $config.editorComponents.editorSyntax.githubuser
-  }
-  else {
-    $githubuser = 'lingua-pupuli'
-  }
-  if ($config.editorComponents.editorSyntax.githubrepo) {
-    $githubrepo = $config.editorComponents.editorSyntax.githubrepo
-  }
-  else {
-    $githubrepo = 'puppet-editor-syntax'
-  }
+task VendorEditorSyntax -precondition { !(Test-Path (Join-Path $PSScriptRoot 'syntaxes/puppet.tmLanguage')) } {
+  $githubrepo = $config.editorComponents.editorSyntax.githubrepo ?? 'puppet-editor-syntax'
+  $githubuser = $config.editorComponents.editorSyntax.githubuser ?? 'puppetlabs'
 
   if ($config.editorComponents.editorSyntax.directory) {
     $source = Join-Path ($config.editorComponents.editorSyntax.directory, 'syntaxes/puppet.tmLanguage')
@@ -99,7 +91,7 @@ task VendorEditorSyntax {
   Invoke-RestMethod -Uri $uri -OutFile $syntaxFilePath -ErrorAction Stop 
 }
 
-task VendorCytoscape {
+task VendorCytoscape -precondition { !(Test-Path (Join-Path $PSScriptRoot 'vendor\cytoscape')) } {
   $cyto = Join-Path $PSScriptRoot 'node_modules\cytoscape\dist'
   $vendorCytoPath = (Join-Path $PSScriptRoot 'vendor\cytoscape')
   Copy-Item -Path $cyto -Recurse -Destination $vendorCytoPath
@@ -113,14 +105,12 @@ task Bump {
   exec { npm version --no-git-tag-version $packageVersion }
 }
 
-task Npm {
+task Npm -precondition { !(Test-Path (Join-Path $PSScriptRoot 'node_modules')) } {
   exec { npm install }
 }
 
 task Vendor -depends VendorEditorServices, VendorEditorSyntax, VendorCytoscape
 
-task Build -depends Clean, Npm, Vendor, CompileTypeScript
-
-task Initial -depends Clean, Vendor
+task Build -depends Npm, Vendor, CompileTypeScript
 
 task default -depends Build
