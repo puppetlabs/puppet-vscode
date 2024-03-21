f19a0c52960513bc2e73530e10292820cbdc05f5
jordanbreen28 <jordan.breen@perforce.com>
2024-03-21 11:57:33 +0000

    (CAT-1510) - refactor settings and handler tests

--- src/test/suite/settings.test.ts
+++ src/test/suite/settings.test.ts
@@ -1,28 +1,30 @@
 import * as assert from 'assert';
 import * as settings from '../../settings';
 
+import { setup, teardown } from 'mocha';
+import * as sinon from 'sinon';
 import * as vscode from 'vscode';
 import * as index from './index';
 
+let sandbox: sinon.SinonSandbox;
+let workspaceConfigurationStub: sinon.SinonStubbedInstance<vscode.WorkspaceConfiguration>;
+
 suite('Settings Tests', () => {
-  const workspaceConfigurationStub = {
-    get: index.sandbox.stub(),
-    has: index.sandbox.stub(),
-    inspect: index.sandbox.stub(),
-    update: index.sandbox.stub(),
-  };
 
-  index.sandbox.stub(vscode.workspace, 'getConfiguration').returns(workspaceConfigurationStub);
+  setup(() => {
+    sandbox = sinon.createSandbox();
+    workspaceConfigurationStub = {
+      get: sandbox.stub(),
+      has: sandbox.stub(),
+      inspect: sandbox.stub(),
+      update: sandbox.stub(),
+    };
+    sandbox.stub(vscode.workspace, 'getConfiguration').returns(workspaceConfigurationStub);
+  });
 
-  const editorServiceSettings: settings.IEditorServiceSettings = {
-    enable: false,
-    timeout: 50,
-  };
-  const pdkSettings: settings.IPDKSettings = {
-    checkVersion: false,
-  };
-  workspaceConfigurationStub.get.withArgs('editorService').returns(editorServiceSettings);
-  workspaceConfigurationStub.get.withArgs('pdk').returns(pdkSettings);
+  teardown(() => {
+    sandbox.restore();
+  });
 
   test('Default settings are populated', () => {
     const defaultWorkspaceSettings = index.defaultSettings;
@@ -30,6 +32,15 @@ suite('Settings Tests', () => {
   });
 
   test('Retrieves settings from workspace', () => {
+    let editorServiceSettings: settings.IEditorServiceSettings = {
+      enable: false,
+      timeout: 50,
+    };
+    let pdkSettings: settings.IPDKSettings = {
+      checkVersion: false,
+    };
+    workspaceConfigurationStub.get.withArgs('editorService').returns(editorServiceSettings);
+    workspaceConfigurationStub.get.withArgs('pdk').returns(pdkSettings);
     const workspaceSettings = settings.settingsFromWorkspace();
     assert.notStrictEqual(workspaceSettings, undefined);
     assert.strictEqual(workspaceSettings.editorService.enable, false);
